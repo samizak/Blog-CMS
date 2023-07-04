8dae6f0082da32336fb6e4b6a9bf540c7f91ab97
Sami Zakir Ahmed <54m1z4k@gmail.com>
2023-07-04 20:29:51 +0100

    Added button to create new articles

--- app/pages/articles.tsx
+++ app/pages/articles.tsx
@@ -24,6 +24,21 @@ export default async function Article() {
         <span>Articles</span>
       </div>
 
+      <div className="flex px-3 py-2 mt-4 text-sm text-white align-middle transition-colors bg-green-600 rounded cursor-pointer hover:bg-green-800 w-fit">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth="1.5"
+          stroke="currentColor"
+          className="w-6 h-6"
+        >
+          <path stroke-linecap="round" stroke-linejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
+        </svg>
+
+        <div className="my-auto">New article</div>
+      </div>
+
       <BlogItem blogTitle={"Test Blog"} blogDate={"04/07/2023"} />
       <BlogItem blogTitle={"Lorem ipsum"} blogDate={"03/07/2023"} />
       <BlogItem blogTitle={"Cool little article!"} blogDate={"02/07/2023"} />
