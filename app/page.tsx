f9a59e66cf09a89a8b743764ed1674a5461e3fa7
Sami Zakir Ahmed <54m1z4k@gmail.com>
2023-07-06 01:16:43 +0100

    Removed comments and unused code

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,9 @@
-import Sidebar from "./components/Sidebar";
-import Dashboard from "./pages/dashboard";
-import Article from "./pages/articles";
-import EditArticle from "./pages/editArticle";
+import Dashboard from "./dashboard/page";
 
 export default async function Home() {
   return (
     <main className="flex flex-row text-black">
-      <Sidebar />
-
-      {/* <Dashboard /> */}
-      <Article />
-      {/* <EditArticle /> */}
+      <Dashboard />
     </main>
   );
 }
